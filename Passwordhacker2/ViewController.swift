75ec026259376b43a46075fb100c597c34a872a6
Maho Misumi <maho.smile606@gmail.com>
2017-11-01 14:57:44 +0900

    完成？

--- Passwordhacker2/ViewController.swift
+++ Passwordhacker2/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var resultLabel1: UILabel!
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    @IBOutlet var resultLabel4: UILabel!
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    var password: Int = 1234
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +29,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func start(){
+        
+        for i in 0...9999 {
+            countLabel.text = String(i)
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.0005))
+            
+            if password == i {
+                 print("正解は%dです！",i)
+                var digits = [Int]()
+                
+                for _ in 0...3{
+                    digits.append(password % 10)
+                    password = password / 10
+                }
+                    resultLabel1.text = String(digits[0])
+                    resultLabel2.text = String(digits[1])
+                    resultLabel3.text = String(digits[2])
+                    resultLabel4.text = String(digits[3])
+            }
+        }
+    }
+    
+    @IBAction func reset() {
+        
+        password = 1234
+        countLabel.text = "startボタンを押して計測開始"
+        
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
+        
+    }
 
 }
-
